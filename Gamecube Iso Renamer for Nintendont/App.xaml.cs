3845454ec7ec66afd826f663cb6cb818478058f1
nikescar <nikescar@hotmail.com>
2019-03-08 23:02:14 -0700

    - initial commit

---
+++ Gamecube Iso Renamer for Nintendont/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Gamecube_Iso_Renamer_for_Nintendont
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
