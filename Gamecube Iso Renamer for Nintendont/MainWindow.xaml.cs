188b4396cfd17f6fb3b2bdd0b4f5d8848bfa603e
Wes Jurica <nikescar@hotmail.com>
2021-08-08 15:12:56 -0600

    Revert "- made game name and directory searching more generic to better suit different romsets"
    This reverts commit 29b9120d58a90025ae0207ddcfe783426bf6101e.

--- Gamecube Iso Renamer for Nintendont/MainWindow.xaml.cs
+++ Gamecube Iso Renamer for Nintendont/MainWindow.xaml.cs
@@ -81,7 +81,7 @@ namespace Gamecube_Iso_Renamer_for_Nintendont
             DirectoryInfo[] subDirectories = dirInfo.GetDirectories();
             bool isDisc2 = false;
 
-            FileInfo[] info = dirInfo.GetFiles("*.iso*", SearchOption.AllDirectories);
+            FileInfo[] info = dirInfo.GetFiles("*.iso*");
             foreach (FileInfo f in info)
             {
                 string curDir = System.IO.Path.GetDirectoryName(f.FullName);
@@ -93,35 +93,24 @@ namespace Gamecube_Iso_Renamer_for_Nintendont
                     Directory.Delete(curDir);
                     string disc1ShortenedDir = disc1Dir.Replace(" (Disc 1)", "");
                     Directory.Move(disc1Dir, disc1ShortenedDir);
-                    Run run = new Run(f.Name + " Disc 2 moved and renamed to disc2.iso \n");
+                    Run run = new Run(System.IO.Path.GetFileName(System.IO.Path.GetDirectoryName(f.FullName)) + " Disc 2 moved and renamed to disc2.iso \n");
                     run.Foreground = Brushes.DarkOliveGreen;
                     statusTextBlock.Inlines.Add(run);
                     //statusText += System.IO.Path.GetFileName(f.FullName) + " moved and renamed to disc2.iso \n";
                 }
                 else
                 {
-                    Console.WriteLine($"{dirInfo.Name} {f.Directory.Name}");
-                    if (f.Directory.Name.Equals(dirInfo.Name))
-                    {
-                        DirectoryInfo newDir = Directory.CreateDirectory(System.IO.Path.Combine(curDir, f.Name.Substring(0,f.Name.Length-4)));
-                        curDir = newDir.FullName;
-
-                        Run run = new Run(f.Name.Substring(0, f.Name.Length - 4) + " Directory Created \n"); 
-                        run.Foreground = Brushes.DarkSeaGreen;
-                        statusTextBlock.Inlines.Add(run);
-                    }
-
                     if (!f.Name.Equals("game.iso") && !f.Name.Equals("disc2.iso"))
                     {
                         File.Move(f.FullName, System.IO.Path.Combine(curDir, "game.iso"));
-                        Run run = new Run(f.Name + " Disc 1 renamed to game.iso \n");
+                        Run run = new Run(System.IO.Path.GetFileName(System.IO.Path.GetDirectoryName(f.FullName)) + " Disc 1 renamed to game.iso \n");
                         run.Foreground = Brushes.DarkGreen;
                         statusTextBlock.Inlines.Add(run);
                         //statusText += System.IO.Path.GetFileName(f.FullName) + " renamed to game.iso \n";
                     }
                     else
                     {
-                        Run run = new Run(f.FullName + " skipped because it already matches naming convention \n");
+                        Run run = new Run(System.IO.Path.GetFileName(System.IO.Path.GetDirectoryName(f.FullName)) + " skipped because it already matches naming convention \n");
                         run.Foreground = Brushes.DarkOrange;
                         statusTextBlock.Inlines.Add(run);
                         //statusText += System.IO.Path.GetFileName(System.IO.Path.GetDirectoryName(f.FullName)) + " skipped because it already matches naming convention \n";
@@ -135,13 +124,13 @@ namespace Gamecube_Iso_Renamer_for_Nintendont
 
             //yield return (double)currentFileCount / (double)fileCount;
 
-            //if (!isDisc2)
-            //{
-            //    foreach (DirectoryInfo directory in subDirectories)
-            //    {
-            //        Rename(directory.FullName);
-            //    }
-            //}
+            if (!isDisc2)
+            {
+                foreach (DirectoryInfo directory in subDirectories)
+                {
+                    Rename(directory.FullName);
+                }
+            }
             /*
             if (currentFileCount == 0)
             {
